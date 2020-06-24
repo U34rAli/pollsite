a13600a1536e412229000c71b7262b3f47d8e5fb
Muhammad Umar <uali10772@gmail.com>
2020-06-25 01:26:24 +0500

    admin interface learnt

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+
 # Create your models here.
 # commented for later use
 # class TimeStampMixin(models.Model):
@@ -16,6 +17,10 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
     def __str__(self):
         return self.question_text
